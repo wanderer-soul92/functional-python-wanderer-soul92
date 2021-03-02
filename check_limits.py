873c5375a0d715b0008d22bafd3ad0547fb622e5
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-03-03 00:32:28 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -3,7 +3,7 @@ import EV_test
 if __name__ == '__main__':
     #assert(EV_test.battery_is_ok(25, 70, 0.7) is True)
     #assert(EV_test.battery_is_ok(50, 85, 0) is False)
-    assert(EV.test.print_out_of_range_output({
+    assert(EV_test.print_out_of_range_output({
        'temperature' : 25,
        'state_of_charge' : 70,
        'charge_rate' : 0.9
